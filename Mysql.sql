c68c41e15230824ab0931218266cd9fd9f79e0cd
Rohit Raj <164794013+codeefy@users.noreply.github.com>
2024-04-26 00:20:46 +0530

    Add files via upload

--- Mysql.sql
+++ Mysql.sql
@@ -1,1 +1,38 @@
-CREATE DATABASE college;
+create DATABASE rkart;
+use rkart;
+Create Table employee(
+id INT PRImary key,
+name varchar (100),
+salary INT 
+);
+
+INSERT INTO employee
+(id,name,salary)
+VALUES 
+(1, "adam",25000),
+(2, "bob",30000),
+(3, "casey", 40000);
+
+SELECT * FROM employee;
+
+CREATE TABLE emp(
+id INT,
+salary int default 25000);
+
+INSERT INTO emp 
+(id)
+VALUES
+(120);
+ SELECT * FROM emp;
+ 
+ CREATE TABLE city(
+ id INT PRIMARY KEY ,
+ city VARCHAR(50),
+ age INT,
+ CONSTRAINT age_check CHECK(age>=18 AND city= "Delhi")
+ );
+ 
+
+
+
+
